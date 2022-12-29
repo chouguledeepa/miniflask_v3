964f6e48297aaa26e375a96ba874fb549fb494b5
python10sep <prashantjamkhande@gmail.com>
2022-12-29 09:25:09 +0530

    changes for singular and plural endpoint

--- crud_ops/starwars.py
+++ crud_ops/starwars.py
@@ -11,13 +11,15 @@ http://127.0.0.1:5000/swapi/planets
 import json
 from flask import Blueprint, Response, Flask
 from models.dal.dml import fetch_resources, fetch_resource
-
+from models.datamodels.films import FilmResponse
 
 crud_app = Blueprint("crud_app", __name__, url_prefix="/swapi")
 
 
 @crud_app.route("/people", methods=["GET"])
 def get_characters():
+
+    # TODO - copy films plural endpoint and implement here
     result = fetch_resources("starwarsDB.characters")
     return Response(json.dumps(result), status=200, mimetype="application/json")
 
@@ -26,8 +28,6 @@ def get_characters():
 @crud_app.route("/films", methods=["GET"])
 def get_films():
     result = fetch_resources("starwarsDB.film")
-
-    from models.datamodels.films import FilmResponse
 
     from pydantic import parse_obj_as
     films = parse_obj_as(list[FilmResponse], result)
@@ -44,32 +44,41 @@ def get_film(index):
         filter_value=index
     )
 
-    from models.datamodels.films import FilmResponse
-    from pydantic import parse_obj_as
-    final = parse_obj_as(list(FilmResponse), result)
+    if result:
+        response_obj = FilmResponse(**result[0])
+        return Response(response_obj.json(), status=200, mimetype="application/json")
+    else:
+        response_obj = {"ERROR": f"No records found for ID - {index}"}
+        return Response(json.dumps(response_obj), status=200, mimetype="application/json")
 
-    return Response(json.dumps(result), status=200, mimetype="application/json")
+
+# TODO : implement singular endpoint for each and every resource
+# planet, character, species, vehicles, starships
 
 
 @crud_app.route("/starships", methods=["GET"])
 def get_starships():
+    # TODO - copy films plural endpoint and implement here
     result = fetch_resources("starwarsDB.starships")
     return Response(json.dumps(result), status=200, mimetype="application/json")
 
 
 @crud_app.route("/vehicles", methods=["GET"])
 def get_vehicles():
+    # TODO - copy films plural endpoint and implement here
     result = fetch_resources("starwarsDB.vehicles")
     return Response(json.dumps(result), status=200, mimetype="application/json")
 
 
 @crud_app.route("/planets", methods=["GET"])
 def get_planets():
+    # TODO - copy films plural endpoint and implement here
     result = fetch_resources("starwarsDB.planets")
     return Response(json.dumps(result), status=200, mimetype="application/json")
 
 
 @crud_app.route("/species", methods=["GET"])
 def get_species():
+    # TODO - copy films plural endpoint and implement here
     result = fetch_resources("starwarsDB.species")
     return Response(json.dumps(result), status=200, mimetype="application/json")
